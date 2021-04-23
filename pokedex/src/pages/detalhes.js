de2e8701b3b5d4388c5b9f4b613ea993dd2534e1
Milena Cacau de Carvalho <milenacacaudecarvalho@gmail.com>
2021-04-22 22:12:19 -0300

    coloquei header em todas as páginas

--- pokedex/src/pages/detalhes.js
+++ pokedex/src/pages/detalhes.js
@@ -1,3 +1,27 @@
+import React, { useContext} from 'react'
+import { useHistory } from "react-router-dom";
+import styled from 'styled-components'
+import {linkDetalhes} from '../routes'
+import Header from '../components/header'
+import { Container, TituloPagina, ListaPokemons, InfosPokemon, BoxCard, BoxImagem, ImagemPokemon, BoxInfos, NomePokemon, CategoriaPokemon, BoxBotoes, BotaoCard} from "../components/estilosCompoentes";
+import GlobalStateContext from '../global/GlobalStateContext'
+import 'antd/dist/antd.css'
+import { List, Card } from 'antd';
+import '../components/antd.css'
+
+
+function Detalhes() {
+    return (
+        <div>
+            <Header/>
+            <Container>
+            </Container>
+        </div>
+    );
+    }
+
+export default Detalhes;
+
 // import React, { useContext, useState, useEffect } from 'react'
 // import { useHistory, useParams } from 'react-router-dom'
 // import axios from 'axios'
